8abc74c3b211028a58823527dd8a3acd8dd26d12
javed2015 <javed2015@users.noreply.github.com>
2022-10-05 22:55:11 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.33.0"
+    }
+  }
+}
+
+
 provider "aws" {
   profile    = "default"
   region = var.region

a251b262c666e40ae9ab6b281ad2d8a5c2cae9c7
javed2015 <javed2015@users.noreply.github.com>
2022-10-05 23:54:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,8 @@ resource "aws_instance" "aws-ec2" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id   = var.subnet_id
+  tags = {
+   name = "ec2-instance by TFC" 
+  }
 
 }
